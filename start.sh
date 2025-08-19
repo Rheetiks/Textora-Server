5c6e291c48c44ea0d1479fba3ffd2305205473ea
Rheetik sharma <127975924+Rheetiks@users.noreply.github.com>
2025-08-19 21:37:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ java -jar /app/textora-backend/target/textora-backend-0.0.1-SNAPSHOT.jar &
 
 echo "Starting Partykit..."
 cd /app/textora-websocket-server
-npx partykit start &
+npx partykit dev --port=1999 &
 
 # Wait for all background processes
 wait
