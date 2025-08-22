74476c949a1b1d8e815a08f501f58da4514f197d
Rheetiks <rheetiksharma6034@gmail.com>
2025-08-22 22:57:03 +0530

    removed partykit run

--- start.sh
+++ start.sh
@@ -2,9 +2,9 @@
 echo "Starting Spring Boot..."
 java -jar /app/textora-backend/target/textora-backend-0.0.1-SNAPSHOT.jar &
 
-echo "Starting Partykit..."
-cd /app/textora-websocket-server
-npx partykit deploy &
+# echo "Starting Partykit..."
+# cd /app/textora-websocket-server
+# npx partykit deploy &
 
 # Wait for all background processes
 wait
